3e4f56f226e6e99b41cfc0bb2d0926cb6322178a
Assi Maimon <maimon33@gmail.com>
2016-07-26 13:40:50 +0300

    Add service costs to history

--- cloudhealth/cost.py
+++ cloudhealth/cost.py
@@ -6,13 +6,14 @@ def _get_current_month():
 
 class CostClient(object):
     CURRENT_COST_URL = '/olap_reports/cost/current'
-    ACCOUNTS_HISTORY_COST_URL = '/olap_reports/custom/893353198679'
+    HISTORY_COST_URL = '/olap_reports/cost/history'
+    CUSTOM_REPORT_URL = '/olap_reports/custom/'
 
     def __init__(self, client):
         self.client = client
 
     def list_months(self, type):
-        response = self.client.get(self.ACCOUNTS_HISTORY_COST_URL)
+        response = self.client.get(self.HISTORY_COST_URL)
 
         list_of_months = []
         months = response['dimensions'][0]["time"]
@@ -35,6 +36,18 @@ class CostClient(object):
 
         return list_of_accounts
 
+    def list_services(self, service='AWS-Service-Category'):
+        response = self.client.get(self.HISTORY_COST_URL)
+
+        list_of_services = []
+        service_list = response['dimensions'][1]['AWS-Service-Category'.encode('ascii')]
+
+        for service in service_list:
+            label = service['label']
+            list_of_services.append(label.encode('ascii'))
+
+        return list_of_services
+
     def get_current(self, account_type='AWS-Account', account_name=None):
         response = self.client.get(self.CURRENT_COST_URL)
 
@@ -56,21 +69,47 @@ class CostClient(object):
             return cost_by_account['Total']
 
     def cost_history(self,
+                     history_report_id='893353198679',
                      account_type='AWS-Account',
                      account_name='Total',
+                     service='Total',
                      month=_get_current_month()):
-        response = self.client.get(self.ACCOUNTS_HISTORY_COST_URL)
+        if history_report_id:
+            response = self.client.get(self.CUSTOM_REPORT_URL + history_report_id)
+        else:
+            response = self.client.get(self.HISTORY_COST_URL)
 
         accounts_cost_by_month = []
-        list_of_months = self.list_months(self.ACCOUNTS_HISTORY_COST_URL)
-        list_of_aws_accounts = self.list_accounts(account_type)
+        service_cost_by_month = []
+        list_of_months = self.list_months(self.HISTORY_COST_URL)
+        if history_report_id:
+            list_of_aws_accounts = self.list_accounts(account_type)
+        else:
+            list_service = self.list_services(account_type)
 
         costs = response['data']
-        for list in costs:
-            costs_history_by_month = dict(zip(list_of_aws_accounts, list))
-            accounts_cost_by_month.append(costs_history_by_month[account_name][0])
+        if history_report_id:
+            for list in costs:
+                costs_history_by_month = dict(zip(list_of_aws_accounts, list))
+                accounts_cost_by_month.append(costs_history_by_month[account_name][0])
 
-        costs_history_for_account = dict(zip(list_of_months,
-                                             accounts_cost_by_month))
+            costs_history_for_account = dict(zip(list_of_months,
+                                                 accounts_cost_by_month))
+        else:
+            print 'Hi'
+            for list in costs:
+                service_cost_history = dict(zip(list_service, list))
+                service_cost_by_month.append(service_cost_history[service][0])
 
-        return costs_history_for_account[month]
+            costs_history_by_service = dict(zip(list_of_months,service_cost_by_month))
+
+        # return costs_history_by_service
+
+        if month  == 'all' and history_report_id:
+            return costs_history_for_account
+        elif service == 'all':
+            costs_history_by_service
+        elif service:
+            costs_history_by_service[service]
+        elif history_report_id:
+            return costs_history_for_account[month]
